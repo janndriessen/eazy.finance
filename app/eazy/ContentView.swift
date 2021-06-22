83bbf10c3e60a9daf2dc820694c10e8c249f4adb
JD <jann.driessen@gmx.de>
2021-06-22 10:33:13 +0200

    add empty app

---
+++ app/eazy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eazy
+//
+//  Created by Jann Driessen on 22.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello world 🕶")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app/eazy/eazyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eazyApp.swift
+//  eazy
+//
+//  Created by Jann Driessen on 22.06.21.
+//
+
+import SwiftUI
+
+@main
+struct eazyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
